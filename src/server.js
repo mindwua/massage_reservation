47da43a66516a2b7df5150b2c28558a8400ded3e
Titi Changpoo <>
2025-02-03 09:56:57 +0700

    Mind | add new router

--- src/server.js
+++ src/server.js
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 import routerAccount from "./routes/account_routes.js";
 import connectDB from "./utils/mongo_utils.js";
 import logger from "./utils/logger_utils.js";
+import routerReservation from "./routes/reservation_routes.js";
 
 dotenv.config({ path: "./src/config/config.env" });
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api/v1", routerAccount);
+app.use("/api/v1", routerAccount, routerReservation);
 
 
 
